79f35c623d672ab94d57ab2478ffb5ddb3780113
Bryan Martinez-Barrales <39033731+bMart666@users.noreply.github.com>
2020-03-11 10:59:34 -0400

    Add files via upload

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,5 @@
 function loadBooks() {
-  var xttp = new XMLHttpRequest();
+  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       var books = this.responseText;
